98e7bbdc466fbd3f715ed7619146aac48d1efdfb
Swend <politra59@bk.ru>
2024-08-17 01:51:10 +0300

    base

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] CharacterController characterController;
+    [SerializeField] float movementSpeed;
+    [SerializeField] float cameraYabsClamp;
+    [SerializeField] float mouseSensetivity;
+    float yRotation = 0;
+    Vector3 movementDirection;
+    const float gravity = 0.1f;
+    float dynamicGravity;
+    bool isGround;
+    private void Update() 
+    {
+        movementDirection = new Vector3(Input.GetAxis("Horizontal"),-dynamicGravity,Input.GetAxis("Vertical"));
+        // float xAxis = Input.GetAxis("Mouse X")*mouseSensetivity;
+        // float yAxis = Input.GetAxis("Mouse Y")*mouseSensetivity;
+        // yRotation -= yAxis;
+        // yRotation = Mathf.Clamp(yRotation,-cameraYabsClamp,cameraYabsClamp);
+        // Camera.main.transform.localEulerAngles = Vector3.right*cameraYabsClamp;
+        // Debug.Log(yAxis);
+    }
+
+    private void FixedUpdate() 
+    {   
+        if(characterController.isGrounded)
+        {
+            dynamicGravity = 0;
+        }
+        else
+        {
+            dynamicGravity += gravity;
+        }
+        characterController.Move(movementDirection * movementSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/TestScale.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScale : MonoBehaviour
+{
+    [SerializeField] float scaleSpeed;
+    [SerializeField] float minimumScale;
+    float scaleValue = 0;
+    List<Transform> touchingObjects = new List<Transform>();
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        touchingObjects.Add(other.transform);
+    }
+
+    private void OnCollisionExit(Collision other) 
+    {
+        touchingObjects.Remove(other.transform);
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Q) && touchingObjects.Count <=2)
+        {
+            scaleValue += scaleSpeed * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.E))
+        {
+            scaleValue -= scaleSpeed * Time.deltaTime;
+        }
+        else
+        {
+            scaleValue = 0;
+        }
+    }
+    private void FixedUpdate() 
+    {
+        if(transform.localScale.x + scaleValue < minimumScale) return;
+        transform.localScale = new Vector3(transform.localScale.x + scaleValue,transform.localScale.x + scaleValue,transform.localScale.x + scaleValue);
+    }
+}
